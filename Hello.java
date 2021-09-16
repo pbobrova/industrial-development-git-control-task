c8b6c398e787110ff4b30f7b94310384217742db
Polina Bobrova <bobrrrovapolina@gmail.com>
2021-09-16 10:28:39 +0300

    add \n

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
- 	System.out.println("New Feature!");
+ 	System.out.println("New Feature!\n");
     }
 }
